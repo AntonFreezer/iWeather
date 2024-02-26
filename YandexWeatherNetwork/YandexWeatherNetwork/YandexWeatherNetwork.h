f6b322b31b9a6fe7a0f75b9b39f24f67ec2c4257
AntonFreezer <anton.kholodkov@gmail.com>
2024-02-26 22:10:30 +0300

    add gitignore, README.md

---
+++ YandexWeatherNetwork/YandexWeatherNetwork/YandexWeatherNetwork.h
@@ -0,0 +1,18 @@
+//
+//  YandexWeatherNetwork.h
+//  YandexWeatherNetwork
+//
+//  Created by Anton Kholodkov on 26.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YandexWeatherNetwork.
+FOUNDATION_EXPORT double YandexWeatherNetworkVersionNumber;
+
+//! Project version string for YandexWeatherNetwork.
+FOUNDATION_EXPORT const unsigned char YandexWeatherNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YandexWeatherNetwork/PublicHeader.h>
+
+
